de0e836c1fe926179673fd58d8afb66843fe3c43
Milhane CHIHEB <70352099+chmilhane@users.noreply.github.com>
2025-12-14 21:25:09 +0100

    Search bar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,46 @@
 export default function Navbar() {
   return (
-    <div className="flex flex-col w-full">
-      <div className="bg-primary shadow-md h-18 flex items-center px-8">
+    <div className="flex flex-col w-full items-center bg-primary shadow-md">
+      <div className="h-18 flex items-center justify-between w-full px-8 max-w-7xl">
+        <div></div>
+        <div className="flex justify-center items-center">
+          <div
+            className="
+              flex
+              ring-1 ring-white/15
+              rounded-sm
+              focus-within:ring-1
+              focus-within:ring-blue-500
+              transition
+            "
+          >
+            <input
+              type="text"
+              placeholder="Rechercher..."
+              className="
+                w-80 px-4 py-2
+                bg-tertiary
+                rounded-l-sm
+                focus:outline-none
+              "
+            />
+
+            <button
+              className="
+                bg-blue-600 hover:bg-blue-700
+                cursor-pointer
+                text-white
+                px-4 py-2
+                rounded-r-sm
+                focus:outline-none
+              "
+            >
+              <img src="./icons/search.png" alt="Search" className="h-4 w-4" />
+            </button>
+          </div>
+        </div>
       </div>
+
       <div className="h-px bg-linear-to-r from-transparent via-white to-transparent opacity-50 w-full"></div>
     </div>
   );
